e9d9805a9d91b90b817a8621231ec86fcbb4d7ba
Igor <izzy0909@gmail.com>
2013-01-16 21:40:23 +0100

    proba samo

--- data_base_access/connection.php
+++ data_base_access/connection.php
@@ -7,7 +7,7 @@ $password=""; // Mysql password
 try {
     $conn = new PDO('mysql:host=localhost;dbname=agencija' . ';charset=UTF8', $username, $password, array(PDO::MYSQL_ATTR_INIT_COMMAND => "SET NAMES utf8"));
     $query = $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-    //$con = new PDO('mysql:host=' . $server . ';dbname=' . $db . ';charset=UTF8', $user, $pass, array(PDO::MYSQL_ATTR_INIT_COMMAND => "SET NAMES utf8"));
+    
 } catch(PDOException $e) {
     echo 'ERROR: ' . $e->getMessage();
 }
