be98a3f6805616ece7f440908c1b0a0fa2e72ee5
Aleksandar Erić <str4qs@gmail.com>
2013-01-22 02:33:17 +0100

    Proba CSS za kursnu listu

--- site/includes/footer.php
+++ site/includes/footer.php
@@ -31,12 +31,12 @@
                         <li><a href="#"><img src="images/soc-fb.png" width="22" alt="Lajkujte nas na Fejsbuku!"> Facebook</a></li>
                         <li><a href="#"><img src="images/soc-tw.png" width="22" alt="Pratite nas na Tviteru!"> Twitter</a></li>
                         <li><a href="#"><img src="images/soc-ms.png" width="22" alt="Budimo prijatelji na Majspejsu!"> Myspace</a></li>
-                        <li><a href="#"><img src="images/soc-tw.png" width="22" alt="Nađite nas na Linkedin-u!"> Linkedin</a></li>
+                        <li><a href="#"><img src="images/soc-li.png" width="22" alt="Nađite nas na Linkedin-u!"> Linkedin</a></li>
                     </ul>
                 </article>
                 <article class="grid_4">
                 	<div class="foot-page2">
-                    <iframe src="http://www.kursna-lista.info/resources/kursna-lista.php?format=3&datum=1" width="260px" height="130px" frameborder="0" scrolling="no"></iframe>
+                    <iframe src="http://www.kursna-lista.info/resources/kursna-lista.php?format=3&datum=1css=http://localhost/agencija/site/css/kl.css" width="260px" height="130px" frameborder="0" scrolling="no"></iframe>
                     </div>
                 </article>
             </div>
