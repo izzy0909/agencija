509cda398de57d84389d4122212b7fb0e0cb7c80
Igor Martinovic <izzy0909@gmail.com>
2013-02-23 17:52:44 +0100

    slikeDA

--- data_base_access/slikeDA.php
+++ data_base_access/slikeDA.php
@@ -103,6 +103,48 @@ function dodajSlikuUponude($NewImageName, $thumb_NewImageName, $stan_id, $DestRa
 
 }
 
+function dodajSlikuSaIzmeni($NewImageName, $thumb_NewImageName, $stan_id, $DestRandImageName, $thumb_DestRandImageName, $watermark_image_name, $watermark_destination)
+{
+    global $conn;
+
+    $sql = "INSERT INTO slike (id, naziv, stan_id, putanja, glavna, vrsta)
+            VALUES              ('', :naziv, :stan_id, :putanja, '0', 'velika')";
+    $query = $conn->prepare($sql);
+    $query->execute(array(
+		':naziv' => $NewImageName,
+		':stan_id' => $stan_id,
+		':putanja' => $DestRandImageName
+        ));
+	
+	$sql = "INSERT INTO slike (id, naziv, stan_id, putanja, glavna, vrsta)
+            VALUES              ('', :naziv, :stan_id, :putanja, '0', 'mala')";
+    $query = $conn->prepare($sql);
+    $query->execute(array(
+		':naziv' => $thumb_NewImageName,
+		':stan_id' => $stan_id,
+		':putanja' => $thumb_DestRandImageName
+        ));
+		
+	$sql = "INSERT INTO slike (id, naziv, stan_id, putanja, glavna, vrsta)
+            VALUES              ('', :naziv, :stan_id, :putanja, '0', 'watermark')";
+    $query = $conn->prepare($sql);
+    $query->execute(array(
+		':naziv' => $watermark_image_name,
+		':stan_id' => $stan_id,
+		':putanja' => $watermark_destination
+        ));
+    $sql = "SELECT * FROM slike
+            WHERE stan_id = :stan_id";
+    $query = $conn->prepare($sql);
+    $query->execute(array(
+		':stan_id' => $stan_id
+        ));
+    $slike = $query->fetchAll(PDO::FETCH_BOTH);
+
+    
+
+}
+
 function prenesiSlikeZaPonude($id, $stan_id)
 {
     global $conn;
