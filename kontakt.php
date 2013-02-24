23a0ef9c97f3015ee633bcc14a90d54c04c3d61a
Aleksandar Erić <str4qs@gmail.com>
2013-02-24 15:40:17 +0100

    translate u gornjem desnom

--- kontakt.php
+++ kontakt.php
@@ -35,9 +35,9 @@
 <body>
 <!--==============================header=================================-->
 <div id="prevod">
-    <div id="google_translate_element"></div><script type="text/javascript">
+<div id="google_translate_element"></div><script type="text/javascript">
 function googleTranslateElementInit() {
-  new google.translate.TranslateElement({pageLanguage: 'sr', includedLanguages: 'de,en,es,fr,it,ru', layout: google.translate.TranslateElement.InlineLayout.SIMPLE}, 'google_translate_element');
+new google.translate.TranslateElement({pageLanguage: 'sr', includedLanguages: 'de,en,es,fr,it,ru', layout: google.translate.TranslateElement.InlineLayout.SIMPLE, autoDisplay: false}, 'google_translate_element');
 }
 </script><script type="text/javascript" src="//translate.google.com/translate_a/element.js?cb=googleTranslateElementInit"></script>
 </div>
