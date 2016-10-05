cd867d48e4d6b03ca4e22231cf26cb1b9b93cdc2
strajqs <str4jqs@gmail.com>
2016-10-06 01:35:52 +0200

    asd

--- new/parts/navigation.php
+++ new/parts/navigation.php
@@ -27,7 +27,7 @@
                     <div class="navbar__submenu">
                       <ul class="navbar__subnav">
                         <li class="navbar__subitem"><a href="about.php" class="navbar__sublink js-navbar-sub-sublink"><?=$lang['navigation.about.team']?></a></li>
-                        <li class="navbar__subitem"><a href="agent_profile.html" class="navbar__sublink js-navbar-sub-sublink"><?=$lang['navigation.about.terms']?></a></li>
+                        <li class="navbar__subitem"><a href="ugovori/opsti_uslovi_poslovanja_jevtic_nekretnine.pdf" target="_blank" class="navbar__sublink js-navbar-sub-sublink"><?=$lang['navigation.about.terms']?></a></li>
                         <li class="navbar__subitem"><a href="agent_profile.html" class="navbar__sublink js-navbar-sub-sublink"><?=$lang['navigation.about.contracts']?></a></li>
                       </ul>
                     </div>
