703fe0bebb1cea7b5fa47fd534b512cc5aa75003
strajqs <str4jqs@gmail.com>
2017-08-21 23:06:19 +0200

    conn fix

--- data_base_access/connection.php
+++ data_base_access/connection.php
@@ -5,7 +5,7 @@ $username="root"; // Mysql username
 $password=""; // Mysql password
 
 try {
-    $conn = new PDO('mysql:host=localhost;dbname=agencija' . ';charset=UTF8', $username, $password, array(PDO::MYSQL_ATTR_INIT_COMMAND => "SET NAMES utf8"));
+    $conn = new PDO('mysql:host=mysql;dbname=jevticne_agencija' . ';charset=UTF8', $username, $password, array(PDO::MYSQL_ATTR_INIT_COMMAND => "SET NAMES utf8",PDO::MYSQL_ATTR_INIT_COMMAND => 'SET sql_mode=""'));
     $query = $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
     
 } catch(PDOException $e) {
